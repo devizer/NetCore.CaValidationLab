1dc20f4c843c7cd124e7b1d5b8561faa6d8070d5
devizer <devizer@users.github.com>
2019-01-25 04:13:45 +0200

    Adding minor cleanup

--- run.sh
+++ run.sh
@@ -4,14 +4,18 @@ if [[ $(uname -m) == armv7* ]]; then rid=linux-arm; fi;
 if [[ $(uname -m) == aarch64 ]]; then rid=linux-arm64; fi; 
 echo Self-Contained rid is \[$rid\]
 
-cd /tmp
+pushd /tmp
 cd $(mktemp -d)
 git clone https://github.com/devizer/NetCore.CaValidationLab
 cd NetCore.CaValidationLab/CaValidationLab
-dotnet run
+dotnet run -c Release
 dotnet publish -c Release --self-contained -o out -r $rid
 cd out
 ./CaValidationLab
+cd ..
+rm -rf bin obj out
+popd
+
 
 
 
