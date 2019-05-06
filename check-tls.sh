1e213b651ef07ceec654d5c26e46596b81370383
devizer <devizer@users.github.com>
2019-05-06 11:31:06 +0300

    check-tls

--- check-tls.sh
+++ check-tls.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# one liner
+# url=https://raw.githubusercontent.com/devizer/NetCore.CaValidationLab/master/check-tls.sh; (wget -q -nv --no-check-certificate -O - $url 2>/dev/null || curl -ksSL $url) | bash
 rid=linux-x64
 if [[ $(uname -m) == armv7* ]]; then rid=linux-arm; elif [[ $(uname -m) == aarch64 ]]; then rid=linux-arm64; elif [[ $(uname -m) == x86_64 ]]; then rid=linux-x64; fi; if [[ $(uname -s) == Darwin ]]; then rid=osx-x64; fi;
 echo Self-Contained rid is \[$rid\]
@@ -8,6 +10,7 @@ rm -rf $work
 mkdir -p $work
 pushd $work
 dotnet new console --no-restore
+rm -f Program.cs
 url=https://raw.githubusercontent.com/devizer/NetCore.CaValidationLab/master/CaValidationLab/Program.cs; (wget -q -nv --no-check-certificate -O - $url 2>/dev/null || curl -ksSL $url) > Program.cs
 dotnet restore
 time dotnet run -c Release
