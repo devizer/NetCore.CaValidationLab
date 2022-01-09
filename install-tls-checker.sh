ba0b3d2b7b08f7a906e7e778cae38d7744d00468
devizer <devizer@github.users>
2022-01-09 05:24:34 +0200

    added install-tls-checker.sh

--- install-tls-checker.sh
+++ install-tls-checker.sh
@@ -5,22 +5,27 @@ set -e
 set -u
 set -o pipefail
 TARGET_DIR="${TARGET_DIR:-$HOME/bin/check-tls}"
-rid=linux-x64
-if [[ $(uname -m) == armv7* ]]; then rid=linux-arm; elif [[ $(uname -m) == aarch64 ]]; then rid=linux-arm64; elif [[ $(uname -m) == x86_64 ]]; then rid=linux-x64; fi; if [[ $(uname -s) == Darwin ]]; then rid=osx-x64; fi;
-echo Self-Contained rid is \[$rid\]
 
-work=/tmp/check-tls-core
-rm -rf $work
-mkdir -p $work
-pushd $work >/dev/null
-dotnet new console --no-restore
-rm -f Program.cs
-url=https://raw.githubusercontent.com/devizer/NetCore.CaValidationLab/master/CaValidationLab/Program.cs; (wget -q -nv --no-check-certificate -O - $url 2>/dev/null || curl -ksSL $url) > Program.cs
-dotnet restore
-# time dotnet run -c Release
-time dotnet publish -c Release --self-contained -o out -r $rid
-mkdir -p $TARGET_DIR
-cp -r -f out/ $TARGET_DIR
-rm -rf bin obj out
-popd >/dev/null
-rm -rf $work
+function install_tls_checker() {
+    rid=linux-x64
+    if [[ $(uname -m) == armv7* ]]; then rid=linux-arm; elif [[ $(uname -m) == aarch64 ]]; then rid=linux-arm64; elif [[ $(uname -m) == x86_64 ]]; then rid=linux-x64; fi; if [[ $(uname -s) == Darwin ]]; then rid=osx-x64; fi;
+    echo Self-Contained rid is \[$rid\]
+
+    work=/tmp/check-tls-core
+    rm -rf $work
+    mkdir -p $work
+    pushd $work >/dev/null
+    dotnet new console --no-restore
+    rm -f Program.cs
+    url=https://raw.githubusercontent.com/devizer/NetCore.CaValidationLab/master/CaValidationLab/Program.cs; (wget -q -nv --no-check-certificate -O - $url 2>/dev/null || curl -ksSL $url) > Program.cs
+    dotnet restore
+    # time dotnet run -c Release
+    time dotnet publish -c Release --self-contained -o out -r $rid
+    mkdir -p $TARGET_DIR
+    cp -r -f out/ $TARGET_DIR
+    rm -rf bin obj out
+    popd >/dev/null
+    rm -rf $work
+}
+
+install_tls_checker
