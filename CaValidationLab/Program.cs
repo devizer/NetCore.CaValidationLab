650373aecd1720cde1b6c929eb21eab155d73ba6
devizer <devizer@github.users>
2022-01-09 16:19:35 +0200

    Added persistent report

--- CaValidationLab/Program.cs
+++ CaValidationLab/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Net.Security;
@@ -13,108 +15,131 @@ namespace CheckHttps
 {
     class Program
     {
-        static readonly Stopwatch StartAt = Stopwatch.StartNew();
-        static readonly object SyncLog = new object();
-        static SslProtocols? TheSslProtocols = SslProtocols.Tls | SslProtocols.Tls11 | SslProtocols.Tls12 | SslProtocols.Tls13;
+        private static readonly Stopwatch StartAt = Stopwatch.StartNew();
+        private static readonly object SyncLog = new object();
+
+        static readonly SslProtocols? TheSslProtocols =
+            SslProtocols.Tls | SslProtocols.Tls11 | SslProtocols.Tls12 | SslProtocols.Tls13;
+
+        private static int _errorsCount;
+        private static StringBuilder _report;
+        private static bool _muteLog = true;
+
+        static string ReportDir
+        {
+            get
+            {
+                var raw = Environment.GetEnvironmentVariable("TLS_REPORT_DIR");
+                return string.IsNullOrEmpty(raw) ? "." : raw;
+            }
+        }
+        
 
         static int Main()
         {
             var sites = new[]
             {
                 "google.com", "youtube.com", "facebook.com", "wikipedia.org", "wikipedia.com", "mozilla.com", "usa.gov",
-                "tls-v1-2.badssl.com:1012", "tls-v1-1.badssl.com:1011", "tls-v1-0.badssl.com:1010", 
+                "tls-v1-2.badssl.com:1012", "tls-v1-1.badssl.com:1011", "tls-v1-0.badssl.com:1010",
                 "tls13.1d.pw", // - does not work
                 "raw.githubusercontent.com"
             };
             ThreadPool.SetMinThreads(sites.Length + 4, 1000);
-            PreJit();
-            Log($"Runtime: {RuntimeInformation.FrameworkDescription}", ConsoleColor.DarkGray);
-            Log($"SSL Protocols: {TheSslProtocols}", ConsoleColor.DarkGray);
 
-            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls13 | SecurityProtocolType.Tls;
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 |
+                                                              SecurityProtocolType.Tls13 | SecurityProtocolType.Tls;
 
 
             ParallelOptions op = new ParallelOptions() {MaxDegreeOfParallelism = sites.Length};
-            StringBuilder report = new StringBuilder();
-            int errorsCount = 0;
-            Parallel.ForEach(sites, op, site =>
-            {
-                SslPolicyErrors sslError = SslPolicyErrors.None;
-                using (HttpClientHandler handler = new HttpClientHandler())
-                using (HttpClient httpClient = new HttpClient(handler))
-                {
-                    if (TheSslProtocols.HasValue) handler.SslProtocols = TheSslProtocols.Value;
-                    // handler.SslProtocols = SslProtocols.Tls12;.
-                    // handler.AllowAutoRedirect = true;
-                    handler.ServerCertificateCustomValidationCallback += (message, certificate2, chain, error) =>
-                    {
-                        if (error != SslPolicyErrors.None)
-                            sslError = error;
+            _report = new StringBuilder();
+            _errorsCount = 0;
+            _muteLog = true;
+            Stopwatch sw = Stopwatch.StartNew();
+            TrySite("mozilla.com");
+            sw.Stop();
+            _errorsCount = 0;
+            _report.Clear();
+            StartAt.Restart();
+            _muteLog = false;
+            Log($"Runtime: {RuntimeInformation.FrameworkDescription}", ConsoleColor.DarkGray);
+            Log($"SSL Protocols: {TheSslProtocols}", ConsoleColor.DarkGray);
+            Log($"JIT for http client completed in {sw.ElapsedMilliseconds:n0} msec", ConsoleColor.DarkGray);
+            Parallel.ForEach(sites, op, TrySite);
 
-                        if (true || error != SslPolicyErrors.None)
-                            Log($"SSL Error Status for {site} ({message.RequestUri}): {error}",
-                            error == SslPolicyErrors.None ? ConsoleColor.DarkGreen : ConsoleColor.DarkRed);
-
-                        return true;
-                    };
-
-                    Log($"Starting {site} ...", ConsoleColor.DarkGray);
-                    try
-                    {
-                        HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Get, $"https://{site}");
-                        var response = httpClient.SendAsync(req).Result;
-                        if (site == "wikipedia.com" && Debugger.IsAttached) Debugger.Break();
-                        string status = sslError == SslPolicyErrors.None ? "" : $", {sslError}";
-                        var reportLine =
-                            $"HTTP Status for {site}: {(int) response.StatusCode} ({response.StatusCode}){status}";
-                        Log(reportLine, ConsoleColor.White);
-                        report.AppendLine(reportLine);
-                    }
-                    catch (Exception ex)
-                    {
-                        var reportLine = $"Error for {site}: {ex.GetType().Name} {ex.Message}"; // {Environment.NewLine}{ex}
-                        Log(reportLine, ConsoleColor.DarkRed);
-                        report.AppendLine(reportLine);
-                        Interlocked.Increment(ref errorsCount);
-                    }
-                }
-            });
-
-            Log($"TOTAL SUMMARY:{Environment.NewLine}{report}", ConsoleColor.Yellow);
-            return errorsCount;
+            Log($"TOTAL SUMMARY:{Environment.NewLine}{_report}", ConsoleColor.Yellow);
+            return _errorsCount;
         }
 
-        static void PreJit()
+        private static void TrySite(string site)
         {
-            const string site = "mozilla.com";
-            string jitResult;
-            Stopwatch sw = Stopwatch.StartNew();
+            var requestUri = $"https://{site}";
+            var idSite = new Uri(requestUri).Host;
+            StoreReportField(idSite, "site", site);
+            SslPolicyErrors sslError = SslPolicyErrors.None;
             using (HttpClientHandler handler = new HttpClientHandler())
             using (HttpClient httpClient = new HttpClient(handler))
             {
-                handler.AllowAutoRedirect = true;
+                if (TheSslProtocols.HasValue) handler.SslProtocols = TheSslProtocols.Value;
+                // handler.SslProtocols = SslProtocols.Tls12;.
+                // handler.AllowAutoRedirect = true;
                 handler.ServerCertificateCustomValidationCallback += (message, certificate2, chain, error) =>
                 {
+                    if (error != SslPolicyErrors.None)
+                        sslError = error;
+
+                    if (true || error != SslPolicyErrors.None)
+                        Log($"SSL Error Status for {site} ({message.RequestUri}): {error}",
+                            error == SslPolicyErrors.None ? ConsoleColor.DarkGreen : ConsoleColor.DarkRed);
+
                     return true;
                 };
 
+                Log($"Starting {site} ...", ConsoleColor.DarkGray);
                 try
                 {
-                    HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Get, $"https://{site}");
+                    HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Get, requestUri);
                     var response = httpClient.SendAsync(req).Result;
-                    jitResult = $"OK in {sw.ElapsedMilliseconds:n0} milliseconds";
+                    if (site == "wikipedia.com" && Debugger.IsAttached) Debugger.Break();
+                    string status = sslError == SslPolicyErrors.None ? "" : $", {sslError}";
+                    var reportLine =
+                        $"HTTP Status for {site}: {(int) response.StatusCode} ({response.StatusCode}){status}";
+                    Log(reportLine, ConsoleColor.White);
+                    _report.AppendLine(reportLine);
+                    StoreReportField(idSite, "http-status", ((int)response.StatusCode).ToString());
+                    StoreReportField(idSite, "ssl-error", sslError.ToString());
                 }
                 catch (Exception ex)
                 {
-                    jitResult = $"Fail in {sw.ElapsedMilliseconds:n0} milliseconds, {ex.GetType().Name} {ex.Message}";
-                }
+                    var reportLine =
+                        $"Error for {site}: {ex.GetType().Name} {ex.Message}"; // {Environment.NewLine}{ex}
+                    Log(reportLine, ConsoleColor.DarkRed);
+                    _report.AppendLine(reportLine);
+                    Interlocked.Increment(ref _errorsCount);
+                    StoreReportField(idSite, "exception", ex.ToString());
+                    StoreReportField(idSite, "exception-messages", GetExceptionDigest(ex));
 
-                Log($"HttpClient jit status: {jitResult}", ConsoleColor.DarkGray);
+                }
             }
+        }
+
+        static void StoreReportField(string idSite, string name, string value)
+        {
+            if (string.IsNullOrWhiteSpace(ReportDir)) return;
+            var fullName = Path.Combine(ReportDir, $"TLS-Report-{idSite}", $"Field-{name}.storage");
+            try
+            {
+                Directory.CreateDirectory(new FileInfo(fullName).Directory.FullName);
+            }
+            catch
+            {
+            }
+
+            File.WriteAllText(fullName, value ?? "");
         }
 
         static void Log(string message, ConsoleColor fore)
         {
+            if (_muteLog) return;
             double msec = StartAt.ElapsedTicks * 1000d / Stopwatch.Frequency;
             // for colorless terminals
             Action<Action> tryAndForget = (action) =>
@@ -144,5 +169,18 @@ namespace CheckHttps
                 tryAndForget(() => Console.ForegroundColor = prev);
             }
         }
+
+        public static string GetExceptionDigest(Exception ex)
+        {
+            List<string> ret = new List<string>();
+            while (ex != null)
+            {
+                ret.Add("[" + ex.GetType().Name + "] " + ex.Message);
+                ex = ex.InnerException;
+            }
+
+            return string.Join(" --> ", ret);
+        }
+
     }
 }
