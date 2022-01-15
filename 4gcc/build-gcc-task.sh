ae50b8e6325bbaa96d9443c0b8609b849a2f4bc3
devizer <devizer@github.users>
2022-01-15 06:30:38 +0200

    Added test job matrix v2

--- 4gcc/build-gcc-task.sh
+++ 4gcc/build-gcc-task.sh
@@ -5,36 +5,7 @@ export GCCURL="${GCCURL:-https://ftp.gnu.org/gnu/gcc/gcc-8.5.0/gcc-8.5.0.tar.xz}
 Say "TARGET GCC: [$GCCURL], Using GCC: [${USEGCC:-}]"
 Say "Flags: [${FLAGS:-}]"
 
-
-echo '
-Acquire::Check-Valid-Until "0";
-APT::Get::Assume-Yes "true";
-APT::Get::AllowUnauthenticated "true";
-Acquire::AllowInsecureRepositories "1";
-Acquire::AllowDowngradeToInsecureRepositories "1";
-Acquire::CompressionTypes::Order { "gz"; };
-APT::NeverAutoRemove:: ".*";
-APT::Compressor::gzip::CompressArg:: "-1";
-APT::Compressor::xz::CompressArg:: "-1";
-APT::Compressor::bzip2::CompressArg:: "-1";
-APT::Compressor::lzma::CompressArg:: "-1";
-' > /etc/apt/apt.conf.d/99Z_Custom
-
-echo '
-deb http://archive.debian.org/debian/ wheezy main non-free contrib
-deb http://archive.debian.org/debian-security wheezy/updates main non-free contrib
-deb http://archive.debian.org/debian wheezy-backports main non-free contrib
-# stable-jessie/snapshots/X.XX.X
-# deb http://download.mono-project.com/repo/debian stable-wheezy/snapshots/5.10 main
-# deb http://download.mono-project.com/repo/debian wheezy main
-' > /etc/apt/sources.list
-try-and-retry apt-get update -qq
-
-export DEBIAN_FRONTEND=noninteractive
-try-and-retry apt-get install build-essential gettext autoconf automake bison flex help2man wget curl m4 pv sudo less nano ncdu tree -y -qq > /dev/null
-try-and-retry apt-get install libc6-dev* -y -qq > /dev/null
-try-and-retry apt-get install gcc-multilib -y -qq > /dev/null
-Say "Completed system prerequisites"
+prepare_os
 
 if [[ "${USEGCC:-}" != "" ]]; then
   
--- 4gcc/build-gcc-utilities.sh
+++ 4gcc/build-gcc-utilities.sh
@@ -1,5 +1,39 @@
 #!/usr/bin/env bash
 
+function prepare_os() {
+echo '
+Acquire::Check-Valid-Until "0";
+APT::Get::Assume-Yes "true";
+APT::Get::AllowUnauthenticated "true";
+Acquire::AllowInsecureRepositories "1";
+Acquire::AllowDowngradeToInsecureRepositories "1";
+Acquire::CompressionTypes::Order { "gz"; };
+APT::NeverAutoRemove:: ".*";
+APT::Compressor::gzip::CompressArg:: "-1";
+APT::Compressor::xz::CompressArg:: "-1";
+APT::Compressor::bzip2::CompressArg:: "-1";
+APT::Compressor::lzma::CompressArg:: "-1";
+' > /etc/apt/apt.conf.d/99Z_Custom
+
+echo '
+deb http://archive.debian.org/debian/ wheezy main non-free contrib
+deb http://archive.debian.org/debian-security wheezy/updates main non-free contrib
+deb http://archive.debian.org/debian wheezy-backports main non-free contrib
+# stable-jessie/snapshots/X.XX.X
+# deb http://download.mono-project.com/repo/debian stable-wheezy/snapshots/5.10 main
+# deb http://download.mono-project.com/repo/debian wheezy main
+' > /etc/apt/sources.list
+try-and-retry apt-get update -qq
+
+export DEBIAN_FRONTEND=noninteractive
+try-and-retry apt-get install build-essential gettext autoconf automake bison flex help2man wget curl m4 pv sudo less nano ncdu tree -y -qq > /dev/null
+try-and-retry apt-get install libc6-dev* -y -qq > /dev/null
+try-and-retry apt-get install gcc-multilib -y -qq > /dev/null
+Say "Completed system prerequisites"
+
+}
+
+
 function generate_uninstall_this_gcc() {
   file="$1"
   to="$2"
