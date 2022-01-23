e1f5e8204a4502cc854336f7f00f3e018585a63f
devizer <devizer@github.users>
2022-01-23 13:02:02 +0200

    deploy 3

--- 4gcc/deploy-fio-from-container.sh
+++ 4gcc/deploy-fio-from-container.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -o; set -u; set -o pipefail
+set -e; set -u; set -o pipefail
 
 Ldd_Version=$(cat ldd-version.log)
 Say "Ldd_Version: [$Ldd_Version]"
@@ -7,24 +7,24 @@ Say "Ldd_Version: [$Ldd_Version]"
 for dir_ver in $(find . -maxdepth 1 -type d | grep -v -E '^\.$' | sort -V); do
   ver="$(basename "$dir_ver")"
   Say "Version [$ver]"
-  pushd "$dir_ver"
+  pushd "$dir_ver" >/dev/null
   for dir_mode in $(find . -maxdepth 1 -type d | grep -v -E '^\.$' | sort -V); do
-    mode="$(basename "$dirver")"
+    mode="$(basename "$dir_mode")"
     is_shared="False"; [[ "$mode" == *"shared"* ]] && is_shared="True";
     Say "-- Version [$ver]; Mode [$mode]; Is Shared [$is_shared]"
-    pushd "$dir_mode"
+    pushd "$dir_mode" >/dev/null
       configure_result="$(cat configure.result)"
       if [[ "$configure_result" -ne 0 ]]; then
         echo "   skip. configure.result is [$configure_result]"
       elif [[ ! -s fio ]]; then
         echo "   skip. missing fio"
       else
-        Say "Ready to Deploy"
+        Say "++ Ready to Deploy for [$ver --> $mode]"
         for result in *.result; do
-        echo "    $result: (cat "$result")"
+        echo "       $result: (cat "$result")"
         done
       fi
-    popd
+    popd >/dev/null
   done
-  popd
+  popd >/dev/null
 done
